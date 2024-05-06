27190863d7eadbf07490c41a7da8c12d4d855a0e
DarkVisio <brainmacfe@gmail.com>
2024-05-06 16:41:16 -0300

    Atualizado

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,22 +1,25 @@
+
+/**
+ *
+ * @author macom
+ */
 public class Operadores {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
 
         //tipos primitivos
         
         String meuNome = "Carlos Junior";
-        double salarioMinimo = 2500;
 
-        short numeroCurto = 1;
-        int numeroNormal1 = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal1;
 
-        int numero = 5;
 
-        numero = 10;
 
         System.out.print(meuNome);
 
-        final double VALOR_DE_PI = 3.14;
+        
 
 
 

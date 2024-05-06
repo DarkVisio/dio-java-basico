5c11a9554b4bfff94294f4d4be7c44686769cbe3
DarkVisio <brainmacfe@gmail.com>
2024-05-06 18:04:38 -0300

    atualizando

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,24 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matematicas entre numeros inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Carlos Junior
+ * @version 1.0
+ * @since 06/05/2024
+ */
+  public class Calculadora {
+      /**
+     * Este metodo é utilizado para somar dois numeros inteiros.
+     * @param numeroUm este é o segundo parametro do metodo.
+     * @param numeroDois este é o segundo parametro do metodo.
+     * @return int o resultado deste metodo e a soma dos dois numeros.
+     */
+       public int somar(int numeroUm, int numeroDois) {
+           return numeroUm + numeroDois;
+       }
+  }
+
+
---
+++ Calculadora/src/Comentarios.java
@@ -0,0 +1,24 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //Ola eu sou um comentario em uma unica linha.
+
+        /* Ola
+         * Eu sou um comentario
+         * que posso ser mais detalhado
+         * quando necessario
+         */
+    
+        }
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nivel de documentação.
+     * Que incrivel !!!
+     * 
+     */ 
+        public void metodo(){
+
+    }
+
+}
